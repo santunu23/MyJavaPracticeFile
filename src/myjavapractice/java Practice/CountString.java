870a63ba5115b850df03156eb0ef8c0cfc0ed584
Joy <santunu23@gmail.com>
2018-01-13 23:23:28 +0600

    Count string characters.

---
+++ src/myjavapractice/java Practice/CountString.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myjavapractice;
+
+/**
+ *
+ * @author Santunu23
+ */
+public class CountString {
+    public static void main(String[] args){
+      String a="Jobab";
+      int count=1;
+      for(int i=1;i<a.length();i++){
+      count++;
+      }
+        System.out.println("Total string number is : "+count);
+      
+    }
+}
